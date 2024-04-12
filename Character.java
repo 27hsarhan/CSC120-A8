b102a4533cb270f44d03b1edf22b3577bb200a5f
27hsarhan <158182999+27hsarhan@users.noreply.github.com>
2024-04-12 01:17:32 -0400

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Character implements Contract{
+    public Character(){
+      // this.itemsList = new ArrayList < String > ();
+      // this.grab = grab;
+      
+    }
+   public void grab (String item){
+      // this.itemsList.add();
+      System.out.println("Item grabbed!");
+    }
+   public void drop (){
+   this.drop();
+    // this.itemsList.remove();
+    System.out.println("Item dropped!");
+  }
+
+  
+  public static void main(String[] args) {
+    // Character character = Character;
+      System.out.println(character);
+ 
+      // public Character(
+  }
+}
